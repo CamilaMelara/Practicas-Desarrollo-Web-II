7d554a5db2bc7eb76cf4def6498fb1bf26eb8b10
CamilaMelara <20235691@esen.edu.sv>
2025-03-02 14:05:39 -0600

    subiendo parte de la guia

--- practica4/src/App.jsx
+++ practica4/src/App.jsx
@@ -1,7 +1,30 @@
-export const App = () => {
-    return(
+import { useContext } from "react"
+import { BudgetForm } from "./components/BudgetForm"
+import { BudgetStateContext } from "./context/BudgetContext"
+import { BudgetTracker } from "./components/BudgetTracker";
+import ExpenseModal from "./components/ExpenseModal";
+
+function App() {
+    const state=useContext(BudgetStateContext)
+    const isValidBudget= state.budget>0;
+    return (
         <>
-        <h1 className="text-8xl">Control de gastos</h1>
+            <header className="bg-blue-600 py-8 max-h-72">
+                <h1 className="uppercase text-center font-black text-4xl text-white">Planificador de gastos</h1>
+            </header>
+
+            <div className="max-w-3xl mx-auto bg-white shadow-lg rounded-lg mt-10 p-10">
+                {isValidBudget ? <BudgetTracker />: <BudgetForm />}
+            </div>
+
+            {isValidBudget && (
+                <main className="max-w-3xl mx-auto py-10">
+                    <ExpenseModal />
+                </main>
+            )}
+            
         </>
-    )
+    );
 }
+
+export default App;
---
+++ practica4/src/data/categorie.js
@@ -0,0 +1,9 @@
+export const categories = [
+    {id: '1', name: 'Ahorro', icon: 'ahorro'},
+    {id: '2', name: 'Comida', icon: 'comida'},
+    {id: '3', name: 'Casa', icon: 'casa'},
+    {id: '4', name: 'Gastos Varios', icon: 'gastos'},
+    {id: '5', name: 'Ocio', icon: 'ocio'},
+    {id: '1', name: 'Salud', icon: 'salud'},
+    {id: '1', name: 'Susccripciones', icon: 'suscripciones'},
+];
